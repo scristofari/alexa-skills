08996a440a0bc12a8c5cf9c48c88e5dde0e42ced
Sylvain Cristofari <s.cristofari@gmail.com>
2018-06-15 16:13:49 +0200

    update

--- alexa/alexa.go
+++ alexa/alexa.go
@@ -1,1 +1,14 @@
 package alexa
+
+func Speak(message string) (*AlexaResponse, error) {
+	res := AlexaResponse{
+		Version: "1.0",
+		Response: &Response{
+			OutputSpeech: &OutputSpeech{
+				Type: "PlainText",
+				Text: "Hello, Lambda",
+			},
+		},
+	}
+	return &res, nil
+}
